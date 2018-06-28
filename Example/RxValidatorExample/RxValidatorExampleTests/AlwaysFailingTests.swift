707a4e82a055d5d6003ca1b5751746ad43b61a4c
Andrew(유금상) <andrew.q@kakaocorp.com>
2018-06-28 09:18:49 +0900

    try ci build

---
+++ Example/RxValidatorExample/RxValidatorExampleTests/AlwaysFailingTests.swift
@@ -0,0 +1,17 @@
+//
+//  AlwaysFailingTests.swift
+//  RxValidatorExampleTests
+//
+//  Created by 유금상 on 2018. 6. 28..
+//  Copyright © 2018년 유금상. All rights reserved.
+//
+
+import XCTest
+
+class AlwaysFailingTests: XCTestCase {
+    
+    func testExample() {
+        XCTFail()
+    }
+    
+}
