cc86b58a3f8e1cfa7274d4995b10885a5dcb02da
kawoou <kawoou@kawoou.kr>
2018-10-06 00:56:42 +0900

    Support swift package manager and Carthage.

---
+++ Supporing Files/RxValidator.h
@@ -0,0 +1,18 @@
+//
+//  RxValidator.h
+//  RxValidator
+//
+//  Created by Kawoou on 06/10/2018.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxValidator_iOS.
+FOUNDATION_EXPORT double RxValidatorVersionNumber;
+
+//! Project version string for RxValidator_iOS.
+FOUNDATION_EXPORT const unsigned char RxValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxValidator/PublicHeader.h>
+
+
